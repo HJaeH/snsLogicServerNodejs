89f23e7413fd291d9aec9f7fde9e3af8c08a274a
jay <blossome223@gmail.com>
2017-02-10 13:55:54 +0900

    commit again for remote env

--- config.js
+++ config.js
@@ -8,18 +8,18 @@ config.mongodb = {};
 config.redis = {};
 
 config.server.port = process.env.PORT || 3000;//'mongodb://localhost:27017/arture');
-//var redisClient = Promise.promisifyAll(redis.createClient(6379,'localhost'));
+// var redisClient = Promise.promisifyAll(redis.createClient(6379,'localhost'));
 
 //mongoDB configurations
-config.mongodb.host = 'mongodb://localhost'; // local DB
-//config.mongodb.host = 'mongodb://localhost'; // remote DB
+// config.mongodb.host = 'mongodb://localhost'; // local DB
+config.mongodb.host = 'mongodb://192.168.1.208'; // remote DB
 config.mongodb.port = 27017;
 config.mongodb.db = 'arture';
 config.mongodb.password = 'pw';
 
 //redis configurations // 
-config.redis.host = 'localhost'; // local redis
-// config.redis.host = '192.168.1.208'; // remote redis
+// config.redis.host = 'localhost'; // local redis
+config.redis.host = '192.168.1.208'; // remote redis
 config.redis.port = 6379;
 
 module.exports = config;
